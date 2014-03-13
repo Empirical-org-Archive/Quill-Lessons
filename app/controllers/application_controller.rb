29d0be2e109a081178dac7f27537413d329a424a
Quinn Shanahan <q.shanahan@gmail.com>
2014-03-13 16:11:21 -0500

    Tweak module auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,9 +34,17 @@ protected
   end
 
   def authenticate!
-    return true if session[:activity_session_id] == :anonymous
+    return if session[:anonymous] == true
 
-    if session[:access_token].blank? && params[:anonymous].blank? && params[:student].blank? && session[:activity_session_id].blank?
+    # let's remember that the session is anonymous
+    if session[:activity_session_id] == :anonymous
+      session[:anonymous] = true
+      return true
+    end
+
+    # if we are at this point then there should definitely be a user signed in on quill as well as a
+    # session ID that we can use. If the access token is blank let's redirect to get it.
+    if session[:access_token].blank?
       redirect_to oauth_redirect_path(back: request.fullpath)
       return false
     end
