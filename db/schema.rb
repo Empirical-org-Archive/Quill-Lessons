f4e9db7ac64f4b3738d69be9d9a73d53fa09042e
James Cox <james@imaj.es>
2014-08-08 20:13:05 -0400

    housekeeping changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140630225144) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: true do |t|
     t.text     "title"
     t.datetime "created_at", null: false
